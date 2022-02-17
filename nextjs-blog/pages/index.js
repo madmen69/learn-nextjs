26485dd07c2fb3d0e008a954cdfa569358db41c3
madmen69 <rielff@gmail.com>
2022-02-17 14:57:08 +1300

    Update keyword

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powereded by{' '}
           <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
