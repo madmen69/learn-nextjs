855aa0554cc75bef26bbf91dbb12bc1b10812dc2
madmen69 <rielff@gmail.com>
2022-03-04 22:28:31 +1300

    commit2  - modified

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -41,6 +41,7 @@ export default function Home() {
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
+            <h2>commit2</h2>
             <h3>Deploy &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
